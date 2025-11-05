498376550e1f57b6f88d901ef71b6421b82b7234
yyhhenry <yyhhenry@foxmail.com>
2025-11-05 10:28:05 +0800

    Refactor SettingsView to implement color mode selector
    Replaced the empty state UI with a color mode selection dropdown, enhancing user experience by allowing users to switch between light, dark, and system modes.

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
-import { Empty, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty'
-import { Sliders } from 'lucide-vue-next'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { useColorMode } from '@vueuse/core'
+import { SunMoon } from 'lucide-vue-next'
+const { store: mode } = useColorMode()
 </script>
 <template>
-  <Empty>
-    <EmptyHeader>
-      <EmptyMedia variant="icon">
-        <Sliders />
-      </EmptyMedia>
-    </EmptyHeader>
-    <EmptyTitle>Settings</EmptyTitle>
-  </Empty>
+  <div class="flex justify-center">
+    <div class="rounded-lg border p-4 bg-white/5 w-180">
+      <div class="flex items-center gap-4">
+        <span class="text-muted-foreground">Color Mode:</span>
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="outline">
+              <SunMoon />
+              <span class="capitalize">{{ mode }}</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'auto'"> System </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  </div>
 </template>
